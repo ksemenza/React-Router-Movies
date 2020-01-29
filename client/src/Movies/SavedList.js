dc3694b36575f29e465b57aa6638208f9b5d502c
ksemenza27 <ksemenza27@outlook.com>
2020-01-28 20:22:44 -0500

    fixed

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
-const SavedList = props => (
+const SavedList = props => {
+  //console.log(props);
+  return (
   <div className="saved-list">
     <h3>Saved Movies:</h3>
-    {props.list.map(movie => (
-      <span className="saved-movie">{movie.title}</span>
-    ))}
-    <div className="home-button">Home</div>
+    {props.list.map(movie => {
+      return movie;
+    })}
+    <div className="home-button"><NavLink to='/'>Home</NavLink></div>
   </div>
-);
+)};
 
 export default SavedList;
